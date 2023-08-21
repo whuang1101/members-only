4d774462aade5553ac1fd7631536378a045c363a
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-20 22:18:53 -0400

    Add first message

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,9 @@ var express = require('express');
 var router = express.Router();
 const form = require('../controllers/userController');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{ loggedIn: req.user });
-});
+router.get('/', form.get_index);
 
-router.get('/sign-up',function(req, res, next) {
-  res.render('index');
-});
+router.get('/sign-up',form.get_index);
 router.get('/sign-out', form.sign_out)
 router.post("/sign-up", form.sign_up_post);
 router.post("/login", form.login_post)
