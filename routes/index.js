f88e1a40e53fffe6ed1568ec6a6d9c02d874be99
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-22 13:38:17 -0400

    Change some visuals

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,8 @@ router.get('/', form.get_index);
 
 router.get('/sign-up',form.get_index);
 router.get('/sign-out', form.sign_out);
-router.get("/login" , form.get_index);
+router.get("/login", form.get_index);
 router.post("/sign-up", form.sign_up_post);
-
 router.post("/login", form.login_post);
 router.post("/membership", form.become_member_post);
 router.post("/add-message",form.add_message_post);
